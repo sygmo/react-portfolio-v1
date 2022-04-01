c6e03a18401071f4e470ac543f7c569f0c64ac59
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-31 20:36:22 -0500

    created basic skeleton for components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/header/Header'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+    </>
   )
 }
 
