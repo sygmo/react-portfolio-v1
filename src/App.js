b9a2ba5fc24a4cc57d10d6a36ae5a88de03f0312
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-31 21:08:13 -0500

    general styles and media queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   )
 }
