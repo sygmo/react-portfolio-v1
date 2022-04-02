009d51504f16a4f802602296c05c9bb9180fb531
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-02 16:41:58 -0500

    nav styles

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,11 +3,13 @@ import './nav.css'
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 import { BiBookAlt, BiMessageSquareDetail } from 'react-icons/bi'
 import { RiServiceLine } from 'react-icons/ri'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'><AiOutlineHome /></a>
+      <a href='#' className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about'><AiOutlineUser /></a>
       <a href='#experience'><BiBookAlt /></a>
       <a href='#services'><RiServiceLine /></a>
