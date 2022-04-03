079c7cd4b331c24a5706ed388e9b16fd0533e8d1
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-03 15:36:36 -0500

    removed services component

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './nav.css'
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 import { BiBookAlt, BiMessageSquareDetail } from 'react-icons/bi'
-import { RiServiceLine } from 'react-icons/ri'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -12,7 +11,6 @@ const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookAlt /></a>
-      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
