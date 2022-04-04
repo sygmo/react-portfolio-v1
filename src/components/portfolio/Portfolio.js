e57d4d17ade878d88a051ee8bd731f4297340117
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-04 14:07:13 -0500

    portfolio component styles

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -19,48 +19,60 @@ const Portfolio = () => {
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com' className='btn' target='_blank'>Github</a>
+            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
       </div>
     </section>
