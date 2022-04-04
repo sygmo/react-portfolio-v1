2e0a638989626cc39593d70821e994d7590aad1f
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-04 14:19:22 -0500

    refactored portfolio component

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,6 +7,51 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Project Name',
+    github: 'https://github.com',
+    demo: 'https://pokeapi.co/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Project Name',
+    github: 'https://github.com',
+    demo: 'https://pokeapi.co/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Project Name',
+    github: 'https://github.com',
+    demo: 'https://pokeapi.co/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project Name',
+    github: 'https://github.com',
+    demo: 'https://pokeapi.co/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Project Name',
+    github: 'https://github.com',
+    demo: 'https://pokeapi.co/'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Project Name',
+    github: 'https://github.com',
+    demo: 'https://pokeapi.co/'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -14,66 +59,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://pokeapi.co/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
