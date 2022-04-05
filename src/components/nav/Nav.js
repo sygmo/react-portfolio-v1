4b5d45d2d7b2bad1a64862fcc8f60cb2b3ccb895
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-05 11:25:26 -0500

    added portfolio to nav bar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './nav.css'
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 import { BiBookAlt, BiMessageSquareDetail } from 'react-icons/bi'
+import { VscFolderOpened } from 'react-icons/vsc'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -11,6 +12,7 @@ const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookAlt /></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><VscFolderOpened /></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
