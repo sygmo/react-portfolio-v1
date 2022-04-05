ee4033de16a490f07907718c8499624e22e139a0
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-05 13:04:41 -0500

    connected emailjs to form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineMail } from 'react-icons/md'
 import { BsLinkedin } from 'react-icons/bs'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_r61ysrr', 'template_529b3yb', form.current, 'qtRs8SVW7pCv-11TD')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -25,7 +41,7 @@ const Contact = () => {
           </article>
         </div>
 
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' required />
           <input type='email' name='email' placeholder='Your Email' required />
           <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
