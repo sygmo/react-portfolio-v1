fb8b0c488cbe841fa94b885f53853dda726c9054
Sydney Mercier <sydney.mercier91@gmail.com>
2022-04-05 11:44:29 -0500

    created contact component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineMail } from 'react-icons/md'
+import { BsLinkedin } from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>sydney.mercier91@gmail.com</h5>
+            <a href='mailto:sydney.mercier91@gmail.com' target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsLinkedin />
+            <h4>LinkedIn</h4>
+            <h5>Sydney Mercier</h5>
+            <a href='https://www.linkedin.com/in/sydney-mercier/' target='_blank'>Send a message</a>
+          </article>
+        </div>
+
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
